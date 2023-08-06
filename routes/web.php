ef7e46fae23a1fbf6a7bffd26c52c96c472305b6
alfirosyad <21alfi@siswa.smktiannajiyah.sch.id>
2023-08-06 22:22:07 +0700

    Set middleware for authentication

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,11 @@ use App\Http\Controllers\AuthController; // Ditambahkan
 
 Route::get('/', function () {
     return view('home');
-})->name('home');
+})->name('home')->middleware('auth');
 
 Route::prefix('tasks')
   ->name('tasks.')
+  ->middleware('auth') // Ditambahkan
   ->controller(TaskController::class)
   ->group(function () {
     Route::get('/', 'index')->name('index');
@@ -40,13 +41,30 @@ Route::prefix('tasks')
     Route::post('{id}/completed','completed')->name('completed');
   });
 
+//Route::name('auth.')
+//  ->controller(AuthController::class)
+//  ->group(function () {
+//    Route::get('signup', 'signupForm')->name('signupForm');
+//    Route::post('signup', 'signup')->name('signup');
+//    // Tambahkan route-route di bawah
+//    Route::get('login', 'loginForm')->name('loginForm');
+//    Route::post('login', 'login')->name('login');
+//    Route::post('logout', 'logout')->name('logout'); // Ditambahkan
+//  });
+
 Route::name('auth.')
   ->controller(AuthController::class)
   ->group(function () {
-    Route::get('signup', 'signupForm')->name('signupForm');
-    Route::post('signup', 'signup')->name('signup');
-    // Tambahkan route-route di bawah
-    Route::get('login', 'loginForm')->name('loginForm');
-    Route::post('login', 'login')->name('login');
-    Route::post('logout', 'logout')->name('logout'); // Ditambahkan
+    Route::middleware('guest')->group(function () {
+      Route::get('signup', 'signupForm')->name('signupForm');
+      Route::post('signup', 'signup')->name('signup');
+      Route::get('login', 'loginForm')->name('loginForm');
+      Route::post('login', 'login')->name('login');
+    });
+
+    Route::middleware('auth')->group(function () {
+      Route::post('logout', 'logout')->name('logout');
+    });
   });
+
+
