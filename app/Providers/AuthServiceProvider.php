8d80e4461ef48c41b5f913372edd3f217ba0a750
alfirosyad <21alfi@siswa.smktiannajiyah.sch.id>
2023-08-09 09:55:21 +0700

    Get authorize

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Task; // Ditambahkan
+use App\Policies\TaskPolicy; // Ditambahkan
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+      Task::class => TaskPolicy::class, // Ditambahkan
     ];
 
     /**
